d2d9b494341b506e172bf33824ab1e4b5d237d06
soundar <asoundarpandiyan@gmail.com>
2024-02-10 12:50:43 +1300

    Handled Category create using http post api. Handled server validation baed on data annotation and custom model state error Binding error to UI control based on tag helpers

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,5 +17,34 @@ namespace BulkyWeb.Controllers
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //custom validation
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "Display Order should not same as name");
+            }
+
+            if (category.Name is not null && category.Name.ToLower().Contains("test"))
+            {
+                ModelState.AddModelError("", "Category shouldn't have test content in the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -6,9 +7,17 @@ namespace BulkyWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [DisplayName("Category Name")]
+        [StringLength(30, ErrorMessage ="String length should be maximum 30chars")]
         public string Name { get; set; }
+
+        [DisplayName("Category Description")]
         public string Description { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage="Range should be between 1 and 100")]
         public int DisplayOrder { get; set; } 
 
     }
