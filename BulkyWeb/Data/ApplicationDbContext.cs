2be81f5b204d52ac88ab3dd3eb992030abd89ce0
soundar <asoundarpandiyan@gmail.com>
2024-02-09 17:40:33 +1300

    Created Category seed data and binded to the view

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
@@ -7,6 +8,19 @@ namespace BulkyWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Description = "Kid Story Books", DisplayOrder = 1, Name = "Story" },
+                new Category { Id = 2, Description = "SciFI Books", DisplayOrder = 2, Name = "SciFi" },
+                new Category { Id = 3, Description = "Religion Books", DisplayOrder = 3, Name = "Religion" }
+                );
+
+            base.OnModelCreating(modelBuilder);
         }
 
     }
---
+++ BulkyWeb/Migrations/20240209035252_SeedCategoryTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Kid Story Books", 1, "Story" },
+                    { 2, "SciFI Books", 2, "SciFi" },
+                    { 3, "Religion Books", 3, "Religion" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
